8b3dbc9bc79eb1eceae01398d2645bc65cefb7d8
Svitlana-V <68326061+Svitlana-V@users.noreply.github.com>
2022-05-03 18:22:34 +0300

    Update replace2.py

--- replace2.py
+++ replace2.py
@@ -1,9 +1,9 @@
 def replace():
     for i in range (len(a)):
-        if a[i]<0:
-            a[i]=-1
-        elif a[i]>0:
-            a[i]=1
+        if a[i] < 0:
+            a[i] = -1
+        elif a[i] > 0:
+            a[i] = 1
                
     return (a)
 
